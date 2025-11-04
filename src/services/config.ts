7bb48c31b8aa538d53bd20ac1e9274fd86aa5aa5
twopact <artemov9321@mail.ru>
2025-11-04 22:26:28 +0300

    Исправление ошибки

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,6 @@
 const config = {
 	api: {
-		baseUrl: '/api',
+		baseUrl: import.meta.env.VITE_API_BASE_URL,
 	},
 }
 
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	server: {
-		proxy: {
-			'/api': {
-				target: import.meta.env.VITE_API_BASE_URL,
-				changeOrigin: true,
-				secure: false,
-				rewrite: path => path.replace(/^\/api/, ''),
-			},
-		},
-	},
 })
